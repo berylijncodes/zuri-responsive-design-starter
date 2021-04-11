8957ffafcec580085310c375735223c750ab3c40
adeleke5140 <adelekekehinde06@gmail.com>
2021-04-11 15:15:51 +0100

    refactor code that toggles the hamburger menu and navLinks on mobile devices

--- script.js
+++ script.js
@@ -2,14 +2,33 @@ const hamburger = document.getElementById('hamburger');
 const close = document.getElementById('close');
 const navUL = document.getElementById('navLinks');
 
-hamburger.addEventListener('click', () => {
-    navUL.classList.toggle('show');
-    hamburger.classList.toggle('hide');
-});
 
-close.addEventListener('click', () => {
-    navUL.classList.toggle('show');
-    hamburger.classList.toggle('hide');
-});
+
+
+hamburger.addEventListener('click',()=>{
+	hamburger.classList.toggle('hide');
+	navUL.classList.toggle('show');
+	close.classList.toggle('show');
+})
+
+
+close.addEventListener('click',()=>{
+	hamburger.classList.remove('hide');
+	navUL.classList.remove('show');
+	close.classList.remove('show');
+})
+
+// hamburger.addEventListener('click', () => {
+//     navUL.classList.toggle('show');
+//     hamburger.classList.toggle('hide');
+//     close.classList.toggle('show');
+// });
+
+// close.addEventListener('click', () => {
+// 	close.classList.toggle('hide');
+// 	hamburger.classList.remove('hide');
+//     hamburger.classList.toggle('show');
+//     navUL.classList.toggle('hide')
+// });
     
 
